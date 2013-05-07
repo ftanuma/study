9caadab81f9209559ea2fab8827ea6afac979c13
Fumihiko Tanuma <ftanuma@gmail.com>
2013-05-07 22:16:07 +0900

    Add recipe 2.2 code.

---
+++ node/cookbook/section2/recipe2_2/server.js
@@ -0,0 +1,28 @@
+var http = require('http');
+var formidable = require('formidable');
+var form = require('fs').readFileSync('form.html');
+//var querystring = require('querystring');
+//var util = require('util');
+//var maxData = 48 * 1024;// bytes
+
+http.createServer(function (req, res){
+    if(req.method === 'GET'){
+	res.writeHead(200, {'Content-type' : 'text/html'});
+	res.end(form);
+    }
+    else if(req.method === 'POST'){
+//	console.log(req.url);
+//	console.log(req.headers);
+	var incoming = new formidable.IncomingForm();
+	incoming.uploadDir = 'uploads';
+	incoming.on('file', function (field, file) {
+	    if(!file.size){
+		return;
+	    }
+	    res.write(file.name + ' is received\n');
+	}).on('end', function() {
+	    res.end('received all files');
+	});
+	incoming.parse(req);   
+    }
+}).listen(8080);
