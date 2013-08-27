5c6ac7d3b8bd819cf4b97b032437e63fb06e4294
Fumihiko Tanuma <ftanuma@gmail.com>
2013-08-27 10:38:51 +0900

    Modified to use emit().

--- node/cookbook/section5/recipe5_2/server.js
+++ node/cookbook/section5/recipe5_2/server.js
@@ -8,12 +8,18 @@ var plainHttpServer = http.createServer(function (req, res) {
 var io = require('socket.io').listen(plainHttpServer);
 io.set('origins', ['localhost:8080', '127.0.0.1:8080']);
 io.sockets.on('connection', function (socket) {
+    socket.emit('hello', 'socket.io !');
+    socket.on('helloback', function (from) {
+        console.log('Received helloback from ' + from);
+    });
+/*             
     socket.on('message', function (msg) {
         if (msg === 'Hello'){
             console.log('sending back to client');
             socket.send('socket.io !');
         }
     });
+*/
 });
 
             
